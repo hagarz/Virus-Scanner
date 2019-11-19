eb3f5d6b8498da0d83bff0d8d194bc23c211d2df
Hagar <hagarzem@gmail.com>
2019-11-19 22:15:06 +0200

    first commit

---
+++ Scanner.py
@@ -0,0 +1,130 @@
+__author__ = 'Hagar Zemach'
+
+import os, platform, sys
+import VirusTotal
+if platform.platform()[:10] != "Windows-10":
+    raise SystemError('Windows 10 compatible')
+if sys.version_info[0] < 3:
+    raise Exception("Must be using Python 3")
+
+
+def load_ext_list():
+    """ creating a list of executable Windows file extensions"""
+    execution_extensions = []
+    with open("high_risk_file_ext.txt", 'r') as file:
+        for row in file:
+            row = [x.strip() for x in row.split('\t')]
+            if "windows" in str.lower(row[-1]):
+                execution_extensions.append(row[0])
+    file.close()
+
+    with open("more_exec_file_exte.txt", 'r') as f:
+        for row in f:
+            row = [x.strip() for x in row.split('\t')]
+            execution_extensions.append(row[0])
+    f.close()
+
+    return execution_extensions
+
+
+class UsersAndFolders(object):
+    """
+    creating a list of folder paths to be scanned
+    """
+    def __init__(self,path_list):
+
+        self.user_list = []
+        self.path_list = []
+        self.path_list = path_list
+
+        # initialize list of users:
+        self.user_list += [user for user in os.listdir(r'C:\Users')]
+
+        # add user-specific paths to list:
+        for user in self.user_list:
+            self.path_list.extend([
+                r'C:\Users\\' + str(user) + '\AppData\Local',
+                r'C:\Users\\' + str(user) + '\AppData\Roaming',
+                r'C:\Users\\' + str(user) + '\Desktop',
+                r'C:\Users\\' + str(user) + '\Downloads'
+            ])
+
+    def update_users(self):
+        """get all user names and return them in a list os strings"""
+        self.user_list += [user for user in os.listdir(r'C:\Users')]
+
+    def get_users(self):
+        return self.user_list
+
+    def update_paths(self):
+        for user in self.user_list:
+            self.path_list.append(
+                r'C:\Users\\' + str(user) + '\AppData\Local',
+                r'C:\Users\\' + str(user) + '\AppData\Roaming',
+                r'C:\Users\\' + str(user) + '\Desktop',
+                r'C:\Users\\' + str(user) + '\Downloads'
+            )
+
+    def get_paths(self):
+        return self.path_list
+
+
+class Scan(object):
+    """
+    This is the controller.
+    search executable files in folders, then send to virus total to be scanned and fetch report
+    """
+    def __init__(self,paths_list):
+        self.ext_list = load_ext_list()  # loading list of executables extensions
+        self.paths = paths_list  # initializing list of 'suspicious' folders
+        self.tb_scanned = []
+
+    def start_scan(self):
+        self.paths = UsersAndFolders(self.paths).get_paths()  # getting list of paths to 'suspicious' folders
+        self.tb_scanned = search_files(self.paths, self.ext_list)  # calling a method to scan the computer
+
+        # send files to VirusTotal for scanning
+        vt = VirusTotal.VirusTotal()
+        vt.vt_mng(self.tb_scanned[500:700])
+
+
+def search_files(paths, ext_list):
+    """
+    iterate over files in listed folders and subfolders and gather executable file in these locations
+    :param paths:  a list containing paths to folders which may contain infected files
+    :param ext_list: lisr of executable extensions
+    :return: a list of file names
+    """
+    tb_scanned = []
+    for path in paths:
+        for (dirpath, dirnames, filenames) in os.walk(path):
+            for file in filenames:
+                for ext in ext_list:
+                    if ext.lower() == "pyc" or ext.lower() == 'js':
+                        break
+                    l_ext = ext.lower()
+                    l_file = file.lower()
+                    if l_file.endswith("." + l_ext):
+                        tb_scanned.append(os.path.join(dirpath, file))
+                        break
+            #     if len(tb_scanned) > 200:
+            #         break
+            # else:
+            #     continue
+            # break
+    print(f"sending {len(tb_scanned)} to be scanned ")
+
+    return tb_scanned
+
+
+
+if __name__ == '__main__':
+    paths_list = [
+        r"C:\Windows\Temp", r"C:\Temp",r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+    ]
+    Scan(paths_list).start_scan()
+
+
+
+
+
---
+++ VirusTotal.py
@@ -0,0 +1,124 @@
+__author__ = 'Hagar Zemach'
+
+
+import requests, json, time
+
+
+class VirusTotal(object):
+    """
+    checks suspicious file in VirusTotal and fetch the VirusTotal scan results
+        # VirusTotal API is limited 4 requests in 1 minute time frame
+        # Request rate	4 requests/minute
+        # Daily quota	1,000 requests/day
+        # Monthly quota	30,000 requests/month
+    """
+    def __init__(self):
+        self.apiKey = get_key()
+        self.base_url = 'https://www.virustotal.com/vtapi/v2/'
+        self.files_id_dic = {}
+        self.scan_id_list = []
+        self.result = {}
+        self.infected={}
+
+    def vt_mng(self,file_list):
+        self.scan(file_list)  # send files for scanning
+        print(len(self.scan_id_list), "files were scanned\n fetching reports...")
+        self.get_report()  # fetching reports
+
+        for sid in self.result:
+            if int(self.result[sid]['positives']) > 0:
+                self.infected[self.files_id_dic[sid]] = \
+                    [str(round(100*(self.result[sid]['positives'] / self.result[sid]['total']),2))+'%',
+                     self.result[sid]['scanners']]
+
+        print("\n--=SUMMARY=--")
+        print(len(file_list), "files were sent for scanning")
+        print(len(self.scan_id_list), "files were scanned \n",len(self.infected), "infected files were found:")
+        for name in self.infected:
+            print(f"malware detected in {name} with {self.infected[name][0]} of scanners as: {self.infected[name][1]}")
+
+
+    def get_report(self):
+        """
+        Retrieve file scan reports by scan ID returned by scan endpoint:
+        # The resource argument can be also be the MD5, SHA-1 or SHA-256 of a file you want to retrieve
+        recent antivirus report.
+        """
+        url = self.base_url + 'file/report'
+        for id in self.scan_id_list:
+            counter = 0
+            response_code = -2
+            inf_scan = []
+            while response_code == -2:
+                try:
+                    time.sleep(20)
+                    params = {'apikey': self.apiKey, 'resource': id}  # resource: Resource(s) to be retrieved
+                    response = requests.get(url, params=params)
+                    output = response.json()
+                    # If the requested item is still queued for analysis it will be -2
+                    response_code = output['response_code']
+                    print(output['verbose_msg'])
+                # in case of connection error, will print a message and try again
+                except requests.exceptions.ConnectionError:
+                    counter += 1
+                    print(f"Connection Error. trying again #{counter}")
+
+            if output['positives'] > 0:
+                # positives meaning Virus Total reported the file as malware by at least one antivirus
+                for scan in output['scans']:
+                    if output['scans'][scan]['detected'] is True:
+                        # fetching information on malware files from all antivirus reported it as malware
+                        inf_scan.append(output['scans'][scan]['result'])
+
+            self.result[output['scan_id']] = \
+                {'total': output['total'], 'positives': output['positives'], 'scanners':inf_scan}
+
+
+    def scan(self,file_l):
+        """
+        Send a file for scanning with VirusTotal
+        file_l is a list of file names
+        """
+        url = self.base_url + 'file/scan'
+        params = {'apikey': self.apiKey}
+
+        first = True
+        for f in file_l:
+            counter = 0
+            Flag = True
+            if not first:
+                time.sleep(20)
+            while Flag:  # handling ConnectionError
+                try:
+                    files = {'file': (f, open(f, 'rb'))}
+                    response = requests.post(url, files=files, params=params)
+                    first = False
+                    Flag = False
+                except requests.exceptions.ConnectionError:
+                    print(f"Connection Error. trying again #{counter}")
+                    Flag = True
+                    counter += 1
+                    if counter > 6:
+                        Flag = False
+                        break
+                except:
+                    print("an error has occurred with file", f)
+                    Flag = False
+
+            try:
+                reply = response.json()
+                print(reply['verbose_msg'])
+                scan_id = reply["scan_id"]
+                self.scan_id_list.append(scan_id)
+                self.files_id_dic[scan_id] = f
+            except json.decoder.JSONDecodeError:
+                print(response.text)
+
+
+def get_key():
+    """gets api key from file"""
+    with open("APIKey.txt", 'r') as file:
+            key = file.read()
+    file.close()
+    return key
+
